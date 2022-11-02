bbf84df93453559c9c49f880a305d1110a56f66b
Lena2323 <lena.jovicic23@gmail.com>
2022-11-02 09:27:58 +0100

    Created MrFreeze App

---
+++ src/MrFreezeApp.js
@@ -0,0 +1,13 @@
+import './MrFreezeApp.css';
+
+function MrFreezeApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        MrFreeze
+      </header>
+    </div>
+  );
+}
+
+export default MrFreezeApp;
