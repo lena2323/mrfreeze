a5747e0624174d9b3644a15179bf1578ae9767df
Lena2323 <lena.jovicic23@gmail.com>
2022-11-12 17:34:54 +0100

    yup i published me secret uri and now I am crying

---
+++ backend/models/freezer.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const freezerSchema = new Schema ({
+username: { type: String, required: true},
+description: { type: String, required: true},
+volume: { type: Number, required: true},
+date: { type: Date, required: true},
+}, {
+    timestamps: true,
+});
+const Freezer = mongoose.model('Freezer', FreezerSchema);
+
+module.exports = Freezer;
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
@@ -15,8 +18,12 @@ console.log ("MongoDB database connection established successfully");
 });
 
 
-app.use(cors());
-app.use(express.json());
+const freezersRouter = require('./routes/freezers');
+const usersRouter = require('./routes/users');
+
+app.use('/freezers', freezersRouter);
+app.use('/users', usersRouter);
+
 
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
