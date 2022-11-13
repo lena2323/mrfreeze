66e12eb46b0fcca32c3ff91aeea1bf94a4bc1c95
Lena2323 <lena.jovicic23@gmail.com>
2022-11-13 19:51:31 +0100

    creating components, done with ze backend i think

--- src/MrFreezeApp.js
+++ src/MrFreezeApp.js
@@ -1,12 +1,23 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from './Components/NavbarComponent/Navbar';
+import FreezersList from './Components/FreezersListComponent/FreezersList';
+import EditFreezer from './Components/EditFreezerComponent/EditFreezer';
+import CreateFreezer from './Components/CreateFreezerComponent/CreateFreezer';
+import CreateUser from './Components/CreateUserComponent/CreateUser';
+
 
 function MrFreezeApp() {
   return (
-    <div className="App">
-      <Navbar title= 'navbar' />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar title= 'navbar' />
+      </div>
+      <Route path="/" exact component={FreezersList}/>
+      <Route path="/edit/:id" component={EditFreezer}/>
+      <Route path="/create" component={CreateFreezer}/>
+      <Route path="/user" component={CreateUser}/>
+    </Router>
   );
 }
 
