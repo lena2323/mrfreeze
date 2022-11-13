2f4862ddebec209e152f4954bb193d41b5214cb8
Lena2323 <lena.jovicic23@gmail.com>
2022-11-13 18:27:16 +0100

    baby steps

--- backend/routes/freezers.js
+++ backend/routes/freezers.js
@@ -27,4 +27,32 @@ newFreezer.save()
     .catch(err => res.status(400).json('Error:' + err));
 });
 
+
+router.route('/:id').get((req, res) => {
+    Freezer.findById(req.params.id)
+    .then(freezer => res.json(freezer))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Freezer.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Freezer deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Freezer.findById(req.params.id)
+    .then(freezer => {
+        freezer.username = req.body.username;
+        freezer.description = req.body.description;
+        freezer.volume = Number(req.body.volume);
+        freezer.date = Date.parse(req.body.date);
+
+        freezer.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
